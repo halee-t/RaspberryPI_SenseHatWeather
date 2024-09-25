59e397bff1ec0fafab40ce849ca867395bf6b726
halee-t <haleetisler@gmail.com>
2024-09-25 13:28:46 -0400

    basic get_compass() code

--- Tisler_p2.py
+++ Tisler_p2.py
@@ -8,3 +8,7 @@ sense.clear()
 while True:
     time.sleep(1)
     # use the compass sensor
+    north = sense.get_compass()
+    print("North: %s" % north)
+
+    # Maybe we can display the direction we are currently facing?
